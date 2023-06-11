fe4e3818b0c14fc03a5b856b41da57be17024656
Maryam Ismail <maryamismail@Maryams-MacBook-Pro.local>
2023-06-11 22:04:27 +0300

    Create Jamiyah is working

--- routes/jamiyah.js
+++ routes/jamiyah.js
@@ -1,15 +1,14 @@
-const express = require("express");
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
-const jamiyahController = require("../controllers/jamiyah");
-const loggedIn = require("../lib/loggedin");
+const jamiyahController = require('../controllers/jamiyah')
 
 router.get("/jamiyah/home", jamiyahController.jamiyah_home_get);
 router.get("/jamiyah/details", jamiyahController.jamiyah_details_get);
-router.get("/jamiyah/create", loggedIn, jamiyahController.jamiyah_create_get);
-
-router.post("/jamiyah/details", jamiyahController.jamiyah_details_post);
+router.get("/jamiyah/create", jamiyahController.jamiyah_create_get);
+// router.post('/jamiyah/delete', jamiyahController.jamiyah_delete)
+// router.post("/jamiyah/details", jamiyahController.jamiyah_details_post);
 router.post("/jamiyah/create", jamiyahController.jamiyah_create_post);
 
 module.exports = router;
