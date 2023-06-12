1d040c8f47f8caadba33836448acc2d0edba84b9
zainabalanjawy <zainabalanjawy@gmail.com>
2023-06-12 09:00:50 +0300

    Secure pages by loggedIn

--- routes/jamiyah.js
+++ routes/jamiyah.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const jamiyahController = require("../controllers/jamiyah");
 const loggedIn = require("../lib/loggedin");
 
-router.get("/jamiyah/home", jamiyahController.jamiyah_home_get);
-router.get("/jamiyah/details", jamiyahController.jamiyah_details_get);
+router.get("/jamiyah/home", loggedIn,jamiyahController.jamiyah_home_get);
+router.get("/jamiyah/details", loggedIn,jamiyahController.jamiyah_details_get);
 router.get("/jamiyah/create", loggedIn, jamiyahController.jamiyah_create_get);
 
 router.post("/jamiyah/details", jamiyahController.jamiyah_details_post);
