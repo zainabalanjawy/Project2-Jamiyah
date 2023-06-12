7a0e974b538dc412d155789323e6cabaf400e03a
zainabalanjawy <zainabalanjawy@gmail.com>
2023-06-12 18:24:53 +0300

    Update ReadMe

--- routes/jamiyah.js
+++ routes/jamiyah.js
@@ -3,13 +3,13 @@ const express = require('express')
 const router = express.Router()
 
 const jamiyahController = require('../controllers/jamiyah')
-
+const loggedIn = require("../lib/loggedin");
 
 router.get("/jamiyah/home", loggedIn,jamiyahController.jamiyah_home_get);
 router.get("/jamiyah/details", loggedIn,jamiyahController.jamiyah_details_get);
 router.get("/jamiyah/create", loggedIn, jamiyahController.jamiyah_create_get);
 
-router.post("/jamiyah/details", jamiyahController.jamiyah_details_post);
+//router.post("/jamiyah/details", jamiyahController.jamiyah_details_post);
 
 // router.post('/jamiyah/delete', jamiyahController.jamiyah_delete)
 
