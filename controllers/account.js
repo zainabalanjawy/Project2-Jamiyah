04039025dde7619fe8788ff6b2b7d58e72b10694
khalid Janahi <khalidjanahi094@gmail.com>
2023-06-13 13:36:06 +0300

    i worked in update , delete account

--- controllers/account.js
+++ controllers/account.js
@@ -1,64 +1,104 @@
-exports.account_details_get = (req,res)=>{
+const Account = require('../models/account')
+exports.account_details_get = async (req,res)=>{
     //res.send(' Here is account/details')
     //Find account balance of the user id to expand its details
-    res.render('account/details')
+   
+    try{
+      const account =await Account.find({ user: [req.user._id] })
+       res.render('account/details',{account})}
+    catch(error){
+      console.log(error)
+    }
 
 }
 
-exports.account_details_post = (req,res)=>{
-    //Take account details to update/delete account
+exports.account_balance_post = async (req,res)=>{
+      //Take account details to update/delete account
+      try
+      {
+        console.log(req.body);
+          await Account.findByIdAndUpdate(req.body.id,{$set:{balance:req.body.balance}})
+          //another way
+          //await Book.findByIdAndUpdate(req.query.id,req.body)
+          return res.redirect('/account/details')
+      }
+      catch(e)
+      {
+          console.log(`Somthing went wrong!!${e.message}`)
+          res.send(e.message)
+      }
+      
+
+
+
     
-}
+  
 
-exports.account_delete = async (req, res) => {
-    console.log(req.query.id)
-    try {
-        // Try to execute this code
-        await account.findByIdAndDelete(req.query.id)
-        return res.redirect('/account/index')
-    } catch (error) {
-        // Execute this if there is an error
-        console.log(error.message)
-        res.send(error.message)
-    } finally {
-        // Execute this code no matter what
-        console.log('We are in the finally block')
-    }
-}
+            // const balanceByMonth = {};
+    
+      //     transactions.forEach(transaction => {
+      //       const date = new Date(transaction.date);
+      //       const year = date.getFullYear();
+      //       const month = date.getMonth() + 1;
+      //       const key = `${year}-${month.toString().padStart(2, '0')}`;
+        
+      //       if (!balanceByMonth[key]) {
+      //         balanceByMonth[key] = { balance: 0, sum: 0 };
+      //       }
+        
+      //       balanceByMonth[key].balance += transaction.amount;
+      //       balanceByMonth[key].sum += transaction.amount;
+      //     });
+        
+      //     return balanceByMonth;
 
-
-exports.account_detail_get = async (req, res) => {
-    try {
-        const account = await account.findById(req.query.id)
-        res.render('account/detail', {account} )
-    } catch (error) {
-        console.log(error.message)
-        res.send(error.message)
-    }
-}
+    
+        }
 
 
 
-exports.account_edit_get = async (req, res) => {
-    try {
-        const account = await account.findById(req.query.id)
-        res.render('account/edit', {account})
-    } catch (error) {
-        console.log(error.message)
-        res.send(error.message)
-    }
-}
-
-exports.account_edit_post = async (req, res) => {
-    try {
-        console.log(req.body.id)
-        await account.findByIdAndUpdate(req.body.id, req.body)
-        res.redirect('/account/index')
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+        exports.account_delete_post = async (req, res) => {
+          try {
+              // Try to execute this code
+              console.log(req.query.id)
+              await Account.findByIdAndDelete(req.query.id)
+              return res.redirect('/user/profile')
+          } catch (error) {
+              // Execute this if there is an error
+              console.log(error.message)
+              res.send(error.message)
+          } finally {
+              // Execute this code no matter what
+              console.log('We are in the finally block')
+          }
+      }
+   
+        
 
 
+      
+  
+    
+
+
+// function getBalanceByMonth(transactions) {
+//     const balanceByMonth = {};
+    
+//     transactions.forEach(transaction => {
+//       const date = new Date(transaction.date);
+//       const year = date.getFullYear();
+//       const month = date.getMonth() + 1;
+//       const key = `${year}-${month.toString().padStart(2, '0')}`;
+  
+//       if (!balanceByMonth[key]) {
+//         balanceByMonth[key] = { balance: 0, sum: 0 };
+//       }
+  
+//       balanceByMonth[key].balance += transaction.amount;
+//       balanceByMonth[key].sum += transaction.amount;
+//     });
+  
+//     return balanceByMonth;
+//   }
 
 
--- routes/account.js
+++ routes/account.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 const accountController = require('../controllers/account')
 
-router.get("/account/details", accountController.account_details_get);
-router.post("/account/details", accountController.account_details_post);
-
+router.get("/account/details",accountController.account_details_get);
+router.post("/account/details",accountController.account_balance_post);
+router.get("/account/delete",accountController.account_delete_post)
 module.exports = router;
