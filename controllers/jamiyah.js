2bdfe67517afe1f835009a78246a6caa47ea3e9b
Husain Abdulla <hussainalhamad.bh@gmail.com>
2023-06-14 12:19:07 +0300

    edited create jamiyah page, added a history page

--- controllers/jamiyah.js
+++ controllers/jamiyah.js
@@ -1,86 +1,118 @@
-const Jamiyah = require('../models/jamiyah')
-const User = require('../models/user')
+const Jamiyah = require("../models/jamiyah");
+const User = require("../models/user");
 
-exports.jamiyah_home_get = async (req,res)=>{
-    //res.send(' Here is jamiyah/home')
-    try{
-        const jamiyahs = await Jamiyah.find({
-            'participants':{ $in: ([req.user._id])}}
-            )
-        res.render('jamiyah/home',  { jamiyahs })
-    } catch (error) {
-        console.log(error.message)
-        res.send('An error occurred')
-    }
-}
+exports.jamiyah_home_get = async (req, res) => {
+  //res.send(' Here is jamiyah/home')
+  try {
+    const jamiyahs = await Jamiyah.find({
+      participants: { $in: [req.user._id] },
+    });
+    res.render("jamiyah/home", { jamiyahs });
+  } catch (error) {
+    console.log(error.message);
+    res.send("An error occurred");
+  }
+};
 
-exports.jamiyah_create_get = async (req,res)=>{
-    //res.send(' Here is jamiyah/create')
-    const users = await User.find()
-    res.render('jamiyah/create', {users})
-}
+exports.jamiyah_create_get = async (req, res) => {
+  //res.send(' Here is jamiyah/create')
+  const users = await User.find();
+  res.render("jamiyah/create", { users });
+};
 
 exports.jamiyah_create_post = (req, res) => {
-    console.log("here")
-    console.log(req.body)
-    const jamiyah = new Jamiyah(req.body)
-    // jamiyah.participants = req.body.id
-    jamiyah.save()
+  console.log("here");
+  console.log(req.body);
+  const jamiyah = new Jamiyah(req.body);
+  // jamiyah.participants = req.body.id
+  jamiyah
+    .save()
     .then(() => {
-        console.log('your Jamiyah has been saved')
-        res.redirect('/jamiyah/home')
+      console.log("your Jamiyah has been saved");
+      res.redirect("/jamiyah/home");
     })
     .catch((err) => {
-        console.log('an error occurred', err)
-    })
-}
+      console.log("an error occurred", err);
+    });
+};
 
-exports.jamiyah_details_get = async (req,res)=>{
-    //res.send(' Here is jamiyah/details')
-    //Find jamaya of the id passed to url and expand its details
-    try{
-        const users = await User.find()
-        const today = new Date();
-        const jamiyah = await Jamiyah.findById(req.query.id)
-        console.log('jam details get: ', jamiyah)
-        res.render('jamiyah/details', {jamiyah, users, today})
-        // console.log(req.query.isEditing)
+exports.jamiyah_details_get = async (req, res) => {
+  //res.send(' Here is jamiyah/details')
+  //Find jamaya of the id passed to url and expand its details
+  try {
+    const users = await User.find();
+    const today = new Date();
+    const jamiyah = await Jamiyah.findById(req.query.id);
+    console.log("jam details get: ", jamiyah);
+    res.render("jamiyah/details", { jamiyah, users, today });
+    // console.log(req.query.isEditing)
 
-        // if (req.query.isEditing === 'true') {
-        //     let isEditing = true
-        // // } else {
-        //     let isEditing = false
-        //     res.render('jamiyah/details', {jamiyah, users, today, isEditing})
-        // }
-    } catch (error) {
-        console.log(error.message)
-        res.send(error.message)
-    }
-}
+    // if (req.query.isEditing === 'true') {
+    //     let isEditing = true
+    // // } else {
+    //     let isEditing = false
+    //     res.render('jamiyah/details', {jamiyah, users, today, isEditing})
+    // }
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+};
 
-exports.jamiyah_details_post = async (req,res)=>{
-    try{
-        console.log(req.body)
-      await Jamiyah.findByIdAndUpdate(req.body.id,req.body)
-        res.redirect('/jamiyah/home')
-    } catch (error) {
-            console.log(error.message)
-            res.send(error.message)
-        }
-}
+exports.jamiyah_details_post = async (req, res) => {
+  try {
+    console.log(req.body);
+    await Jamiyah.findByIdAndUpdate(req.body.id, req.body);
+    res.redirect("/jamiyah/home");
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+};
 
-exports.jamiyah_delete = async(req, res) => {
-    console.log(req.query.id)
-    try {
-        // Try to excute this code
-        await Jamiyah.findByIdAndDelete(req.query.id)
-        return res.redirect('/jamiyah/home')
-    } catch (error) {
-        // Execute this if there is an error
-        console.log(error.message)
-        res.send(error.message)
-    } finally {
-        //Execute this code no matter what
-        console.log('We are in the finally block')
-    }
-}
+exports.jamiyah_delete = async (req, res) => {
+  console.log(req.query.id);
+  try {
+    // Try to excute this code
+    await Jamiyah.findByIdAndDelete(req.query.id);
+    return res.redirect("/jamiyah/home");
+  } catch (error) {
+    // Execute this if there is an error
+    console.log(error.message);
+    res.send(error.message);
+  } finally {
+    //Execute this code no matter what
+    console.log("We are in the finally block");
+  }
+};
+
+exports.jamiyah_history = (req, res) => {
+  res.render("jamiyah/history");
+};
+
+// exports.jamiyahs_home_filter = async (req, res) => {
+//   try {
+//     const jamiyahs = await Jamiyah.find();
+
+//     const homeJamiyahs = jamiyahs.filter(
+//       (jamiyah) => jamiyah.path === "/jamiyah/home"
+//     );
+
+//     res.render("jamiyah/home", { homeJamiyahs });
+//   } catch (error) {
+//     res.status(500).render("error", { message: error.message });
+//   }
+// };
+// exports.jamiyahs_history_filter = async (req, res) => {
+//   try {
+//     const jamiyahs = await Jamiyah.find();
+
+//     const historyJamiyahs = jamiyahs.filter(
+//       (jamiyah) => jamiyah.path === "/jamiyah/history"
+//     );
+
+//     res.render("jamiyah/history", { historyJamiyahs });
+//   } catch (error) {
+//     res.status(500).render("error", { message: error.message });
+//   }
+// };
